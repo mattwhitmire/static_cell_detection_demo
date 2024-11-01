051b895df577920f8e151595001f10b36c597d2e
qliu-Inscopix <79870100+qliu-Inscopix@users.noreply.github.com>
2024-11-01 16:58:12 +0000

    disable token

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -6,5 +6,5 @@ Xvfb :99 -screen 0 1280x720x24 &
 # Export DISPLAY variable
 export DISPLAY=:99
 
-# Start JupyterLab
-jupyter --ip 0.0.0.0 --no-browser --allow-root
+# Start JupyterLab without token authentication
+jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
